de633dee8b2394ef7287d38b16a2c77de6c21a68
vlacertosa <vlacertosa.ptech@gmail.com>
2015-12-17 10:56:51 -0500

    k

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var main = function() {
 
+$('.dropdown-toggle').click(function() {
+  $('.dropdown-menu').toggle ();
+});
+$('.dropdown-menu').hide();
+
+
+
+}
 
 
 
 
-};
+
 
 $(document).ready(main);
